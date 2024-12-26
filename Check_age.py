0e89e00d417a148c2a98c82b012de987b87199a3
THSayeb <desktop499@gmail.com>
2024-12-26 18:32:58 +0600

    Check age

---
+++ Check_age.py
@@ -0,0 +1,7 @@
+print("\nTeenager association \nHere, \nWe discuss about many social works that is done by us all around the world")
+age = int(input("________________________\nFirst,\nEnter your age in interger "))
+
+if age >= 10 and age<= 20:
+    print("You are eligible \nChick here to enter our association")
+else:
+    print("Sorry,\nYou can't access our inner works")
